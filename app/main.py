dc76dc64197e86f804ee13ce81dd6c8703c6c451
Rob Ashton <robertashton94@gmail.com>
2025-07-08 10:15:11 +0100

    Add WIP cosmos DB impl

--- app/main.py
+++ app/main.py
@@ -1,8 +1,41 @@
-from fastapi import FastAPI
+from functools import lru_cache
+from fastapi import Depends, FastAPI
+from typing_extensions import Annotated
+from azure.cosmos import ContainerProxy, CosmosClient
+
+from . import config
+from . import cosmos
+
 
 app = FastAPI()
+
+
+@lru_cache
+def get_settings():
+    return config.Settings()
+
+
+@lru_cache
+def get_cosmos_client():
+    settings = get_settings()
+    return CosmosClient(
+        url=settings.cosmos_url,
+        credential=settings.cosmos_key
+    )
+
+
+@lru_cache
+def get_container() -> ContainerProxy:
+    client = get_cosmos_client()
+    return cosmos.setup_cosmos(client, db_id="mydb", container_id="default")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/write")
+async def write_cosmos(container: Annotated[ContainerProxy, Depends(dependency=get_container)]):
+    _res = container.upsert_item({"key": "my item 123"})
+    return {"message": "success"}
--- tests/test_api.py
+++ tests/test_api.py
@@ -9,3 +9,24 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_write_cosmosdb():
+    import certifi
+    import os
+    print("running tests")
+    print(certifi.where())
+    print("request CA bundle")
+    print(os.environ.get("REQUESTS_CA_BUNDLE"))
+    print("curl CA bundle")
+    print(os.environ.get('CURL_CA_BUNDLE'))
+    import requests
+    print("Cert path used:", certifi.where())
+    try:
+        r = requests.get("https://localhost:8081/_explorer/index.html", verify=certifi.where())
+        print("Status:", r.status_code)
+    except requests.exceptions.SSLError as e:
+        print("SSL ERROR:", e)
+    response = client.post("/write")
+    assert response.status_code == 200
+    assert response.json() == {"message": "success"}
