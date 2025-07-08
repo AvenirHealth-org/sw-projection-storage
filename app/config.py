73421c23e5fd434efeb43c64b8ebea33e548d6d7
Rob Ashton <robertashton94@gmail.com>
2025-07-08 17:05:18 +0100

    Fix pyright and lint warnings

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config: SettingsConfigDict = SettingsConfigDict(env_file=".env")
+    model_config: SettingsConfigDict = SettingsConfigDict(env_file=".env") # pyright: ignore[reportIncompatibleVariableOverride]
     app_name: str = "sw-projection-storage"
     cosmos_url: str = "xxx"
     cosmos_key: str = "xxx"
